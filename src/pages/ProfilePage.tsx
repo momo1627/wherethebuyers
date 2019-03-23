4689e8eb75369f744b2afea3edd295a0d8057d0c
yxx0410 <yxx.learning@gmail.com>
2019-03-23 18:09:24 +1100

    init feature/mytasks

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -4,7 +4,6 @@ import {Link} from 'react-router-dom'
 import {SignInStatus,Update} from '../middleware/context'
 import useGetData from '../middleware/customHooks/useGetData'
 import {startUpdate,endUpdate} from '../middleware/actions/updateAction'
-<<<<<<< HEAD:src/pages/ProfilePage.tsx
 type Props = {
     match:{
         params:
@@ -15,12 +14,6 @@ const Profile:React.FunctionComponent<Props> = (props)=>{
     const {signInStatus} = React.useContext(SignInStatus);
     // const [postedTask,fetchStatus,dispatch] = useGetData(`http://localhost:5000/tasks?poster=${props.match.params.id}`);
     // const [assignedTask] = useGetData(`http://localhost:5000/tasks?assignTo=${props.match.params.id}`)
-=======
-const Profile = (props)=>{
-    const [signInStatus] = useContext(SignInStatus);
-    const [postedTask,dispatch] = useGetData([],`http://localhost:5000/tasks?poster=${props.match.params.id}`);
-    const [assignedTask] = useGetData([],`http://localhost:5000/tasks?assignTo=${props.match.params.id}`)
->>>>>>> 9d167765d16a6ecbb97bb1dcd31ef8e2ff9aeb43:src/pages/ProfilePage.jsx
 
     const handleComplete = async(input:string)=>{
        await axios.patch(`http://localhost:5000/tasks/${input}`,{status:`DONE`})
