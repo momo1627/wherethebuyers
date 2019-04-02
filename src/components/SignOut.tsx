62690517f0636715391447a0505af3b598550eff
momo1627 <momo1627@gmail.com>
2019-04-02 19:19:38 +1100

    deploy

--- src/components/SignOut.tsx
+++ src/components/SignOut.tsx
@@ -4,7 +4,7 @@ import {signOutAction} from '../middleware/actions/signInAction'
 const SignOut:React.FunctionComponent = ()=>{
     const {signInStatus,signInDispatch} = React.useContext(SignInStatus)
     const handleSignOut = async ()=>{
-        const result = await fetch('localhost:5000/sign-out',{credentials:"include"})
+        const result = await fetch('http://localhost:5000/sign-out',{credentials:"include"})
     }
     return (
         <div style={{"maxWidth":"240px"}} className="col-6 col-md-4 col-lg-4 btn-group px-1">
--- src/middleware/customHooks/useMyTasks.tsx
+++ src/middleware/customHooks/useMyTasks.tsx
@@ -11,7 +11,7 @@ interface IResponse<T> {
 export default function useMyTasks(role:string,filter:string):[IResponse<Data[]>,boolean]{
     const { signInStatus } = React.useContext(SignInStatus);
     const username = signInStatus.username;
-    const url = filter === 'all'? `http://ec2-3-89-33-101.compute-1.amazonaws.com/mytasks?${role}=${username}` : `http://ec2-3-89-33-101.compute-1.amazonaws.com/mytasks?${role}=${username}&status=${filter}` 
+    const url = filter === 'all'? `http://localhost:5000/mytasks?${role}=${username}` : `http://localhost:5000/mytasks?${role}=${username}&status=${filter}` 
     const [response,fetchStatus,updateDispatch] = useFetchData<Data>(url,{method:'get'});
     updateDispatch(endUpdate)
     return [response as IResponse<Data[]> ,fetchStatus]
