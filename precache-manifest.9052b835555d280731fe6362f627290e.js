9eecd6eeb9492b8682fcb82c98566201e51a51fe
momo1627 <momo1627@gmail.com>
2019-04-16 17:40:42 +1000

    Updates

---
+++ precache-manifest.9052b835555d280731fe6362f627290e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5511a860494d572d7b31",
+    "url": "/wherethebuyers/static/js/runtime~main.5511a860.js"
+  },
+  {
+    "revision": "abe697bb81e1173ae34b",
+    "url": "/wherethebuyers/static/js/main.abe697bb.chunk.js"
+  },
+  {
+    "revision": "a0a95c3f32507e8bbdb1",
+    "url": "/wherethebuyers/static/js/2.a0a95c3f.chunk.js"
+  },
+  {
+    "revision": "abe697bb81e1173ae34b",
+    "url": "/wherethebuyers/static/css/main.9cca5acf.chunk.css"
+  },
+  {
+    "revision": "bfe1000abf71336bd56174f9c09c00ca",
+    "url": "/wherethebuyers/index.html"
+  }
+];
