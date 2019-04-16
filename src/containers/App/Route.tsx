428f9d0ed6847c67dd3e419332200fb15cad6038
momo1627 <momo1627@gmail.com>
2019-04-16 15:53:08 +1000

    deploy

--- src/containers/App/Route.tsx
+++ src/containers/App/Route.tsx
@@ -8,17 +8,15 @@ import ReviewMaker from '../TaskDetail/ReviewMaker'
 const Routes: React.FunctionComponent = () => {
     return (
         <div className='app-content'>
-        <div className='tasks-container'>
-
-            <Switch >
-                <Route exact path='/app' component={BrowseTasks} />
-                <Route exact path='/app/tasks' component={BrowseTasks} />
-                <Route exact path='/app/task/:id' component={TaskDetail} />
-                {/* <Route exact path='/mytasks' component={MyTasks} /> */}
-                <PrivateRoute exact path='/app/mytasks' component={MyTasks} />
-                <Route exact path='/app/review' component = {ReviewMaker} />
-            </Switch>
-        </div>
+            <div className='tasks-container'>
+                <Switch >
+                    <Route exact path='/app/' component={BrowseTasks} />
+                    <Route exact path='/app/tasks' component={BrowseTasks} />
+                    <Route exact path='/app/task/:id' component={TaskDetail} />
+                    <PrivateRoute exact path='/app/mytasks' component={MyTasks} />
+                    <Route exact path='/app/review' component={ReviewMaker} />
+                </Switch>
+            </div>
         </div>
 
     )
