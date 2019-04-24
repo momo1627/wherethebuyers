3a6634141e2b601d4ec1e5498ca7a513d09fb46b
momo1627 <momo1627@gmail.com>
2019-04-25 07:01:53 +1000

    dns

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
-const API_Url = 'http://localhost:5000/api'
+// const API_Url = 'http://localhost:5000/api'
 // const API_Url = 'http://ec2-3-89-33-101.compute-1.amazonaws.com/api'
+const API_Url = 'http://www.wherethebuyers.theyxx0410.net'
 export default API_Url
